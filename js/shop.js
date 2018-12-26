9de3b5ae82dda8460654f72bc6b7e07eb8774882
Дмитрий <proton0993ak@gmail.com>
2018-12-26 16:50:51 +0300

    c5

--- js/shop.js
+++ js/shop.js
@@ -1,27 +1,84 @@
-$(document).ready(function(){
+// $(document).ready(function(){
     
-    $.getJSON("https://spreadsheets.google.com/feeds/list/1gWrihXlnVe57w37Wo-SmN55tAbjs4zJb4RFIFBjzAB4/od6/public/values?alt=json",
-    function(data){
-        data = data['feed']['entry']
-        console.log(data);
-        showGoods(data);
-    })  
+//     $.getJSON("https://spreadsheets.google.com/feeds/list/1gWrihXlnVe57w37Wo-SmN55tAbjs4zJb4RFIFBjzAB4/od6/public/values?alt=json",
+//     function(data){
+//         data = data['feed']['entry']
+//         console.log(data);
+//         showGoods(data);
+//     })  
 
-     function showGoods(data){
-                var out = '';
-                for(var i=0; i<data.length; i++){
-                    if(data[i]['gsx$show']['$t'] != 0){
-                out += ` <div class="col-lg-3 col-md-3 col-sm-2 text-center">`
-                out += ` <div class="goods">`
-                out += ` <h5 class="name">${data[i]['gsx$name']['$t']}</h5>`
-                out += ` <img src="${data[i]['gsx$image']['$t']}" alt="">`
-                out += ` <p class="sum">На складе: ${data[i]['gsx$kg']['$t']} кг</p>`
-                out += ` <p class="cost">Цена: <span class="cost_style">${data[i]['gsx$cost']['$t']} </span>руб.</p>`
-                out += ` </div>`
-                out += ` </div>`
+window.onload = function(){
+    // послать запрос
+
+    let getJSON = function(url, callback){
+        let xhr = new XMLHttpRequest();
+        xhr.open('GET', url, true);
+        xhr.responseType= 'json';
+        xhr.onload = function(){
+            let status = xhr.status;
+            if(status == 200){
+                callback(null, xhr.response)    
+               
+                
             }
-                }
-                $('.shop-field').html(out);
-     }
-})
+            else{
+                callback(status,xhr.response);
+            }
+        };
+        xhr.send();
+        
+    }
+
+    getJSON('https://spreadsheets.google.com/feeds/list/1gCFewzakIekxL5P1s74TZ055SNalSHXiw-Bkf-N0Tz0/od6/public/values?alt=json', function(err,data){
+        console.log(data); 
+        if(err !== null){
+            alert('Error: ' +  err);
+        }
+        else{
+            data = data['feed']['entry'];
+            console.log(data);
+            document.querySelector('.shop-field').innerHTML = showGoods(data);
+        }
+
+
+    
+    });
+
+    function showGoods(data){
+        var out = '';
+        for(var i=0; i<data.length; i++){
+            if(data[i]['gsx$show']['$t'] != 0){
+        out += ` <div class="col-lg-3 col-md-3 col-sm-2 text-center">`
+        out += ` <div class="goods">`
+        out += ` <h5 class="name">${data[i]['gsx$name']['$t']}</h5>`
+        out += ` <img src="${data[i]['gsx$image']['$t']}" alt="">`
+        out += ` <p class="sum">На складе: ${data[i]['gsx$kg']['$t']} кг</p>`
+        out += ` <p class="cost">Цена: <span class="cost_style">${data[i]['gsx$cost']['$t']} </span>руб.</p>`
+        out += ` <p class="buy"><button class="btn btn-success" data="${data[i]['gsx$id']['$t']}">Купить</button></p>`
+        out += ` </div>`
+        out += ` </div>`
+    }
+        }
+        return out;
+}
+
+}
+
+//      function showGoods(data){
+//                 var out = '';
+//                 for(var i=0; i<data.length; i++){
+//                     if(data[i]['gsx$show']['$t'] != 0){
+//                 out += ` <div class="col-lg-3 col-md-3 col-sm-2 text-center">`
+//                 out += ` <div class="goods">`
+//                 out += ` <h5 class="name">${data[i]['gsx$name']['$t']}</h5>`
+//                 out += ` <img src="${data[i]['gsx$image']['$t']}" alt="">`
+//                 out += ` <p class="sum">На складе: ${data[i]['gsx$kg']['$t']} кг</p>`
+//                 out += ` <p class="cost">Цена: <span class="cost_style">${data[i]['gsx$cost']['$t']} </span>руб.</p>`
+//                 out += ` </div>`
+//                 out += ` </div>`
+//             }
+//                 }
+//                 $('.shop-field').html(out);
+//      }
+// })
 
